8fc939974b66629c2076a2c8a2770521a1dfd9f0
Mooventhan <kmooventhan98@gmail.com>
2021-03-05 13:10:28 +0530

    initial commit

---
+++ src/App.js
@@ -0,0 +1,35 @@
+import React, { useState } from 'react';
+import './App.css';
+import ReactModal from "react-modal"; //yarn add react-modal
+
+ReactModal.setAppElement('#root')
+ function App() {
+   const [modalIsOpen, setModalIsOpen] = useState(false)
+  return (
+    <div className="App">
+          <h1>Open Your Model Here</h1>
+          <button onClick={()=>setModalIsOpen(true)}>Open</button>
+            <ReactModal 
+              isOpen={modalIsOpen} 
+              onRequestClose={()=>setModalIsOpen(false)}//for Esc key and outof click to close
+              shouldCloseOnOverlayClick={false} //disable outof click to close
+              style={
+                {
+                  overlay: {
+                    backgroundColor: 'grey'
+                  },
+                  content: {
+                    content: 'orange'
+                  }
+                }
+              }
+              >
+                <h2>This is modal</h2>
+                <p>This is the content</p>
+                <button onClick={()=>setModalIsOpen(false)}>Close(Esc)</button>
+            </ReactModal>
+      </div>
+  )
+}
+
+export default App
